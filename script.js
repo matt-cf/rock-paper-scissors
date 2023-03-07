b8fe1f4344a86b44421d237f9925eaafa69b6c68
Matthew <cheng.f.lou@gmail.com>
2023-03-08 01:01:09 +0800

    -all

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
 choiceArray = ['Rock', 'Paper', 'Scissors'];
 
+
 function getComputerChoice() {
     random = Math.floor(Math.random() * choiceArray.length);
-    getComputerChoice = choiceArray[random];
-    getComputerChoice = getComputerChoice.toLowerCase()
-    return getComputerChoice;
-    
+    let computerSelection = choiceArray[random];
+    computerSelection = computerSelection.toLowerCase()
+    return computerSelection;
 }
 
-let playerChoice = prompt("Rock, paper, scissors?");
-playerChoice = playerChoice.toLowerCase();
-let computerChoice = getComputerChoice();   
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound() {
+    let playerChoice = prompt("Rock, paper, scissors?");
+    playerChoice = playerChoice.toLowerCase();
+    let computerChoice = getComputerChoice();
     if (playerChoice == computerChoice) {
         console.log("draw");
     } else if (playerChoice == "rock" && computerChoice == "paper") {
-        console.log("you lost");
+        console.log(`You lost. You used ${playerChoice} and the computer used ${computerChoice}.`);
+        computerScore++;
     } else if (playerChoice == "paper" && computerChoice == "scissors") {
-        console.log("you lost");
+        console.log(`You lost. You used ${playerChoice} and the computer used ${computerChoice}.`);
+        computerScore++;
     } else if (playerChoice == "scissors" && computerChoice == "rock") {
-        console.log("you lost");
+        console.log(`You lost. You used ${playerChoice} and the computer used ${computerChoice}.`);
+        computerScore++;
     } else {
-        console.log("you won");
+        console.log(`You won. You used ${playerChoice} and the computer used ${computerChoice}.`);
+        playerScore++;
     }
     return;
 }
 
-
-
+function game() {
+    for (let i = 0; i < 5; i++){
+    playRound();
+    }
+    if (playerScore > computerScore) {
+        console.log(`The player has won the game by winning ${playerScore} round(s)!`)
+    } else if (playerScore > computerScore) {
+        console.log(`The computer has won the game by winning ${computerScore} round(s)!`)
+    } else {
+        console.log(`It is a draw! Both players won ${playerScore} round(s)!`)
+    }
+    playerScore = 0;
+    computerScore = 0;
+}
